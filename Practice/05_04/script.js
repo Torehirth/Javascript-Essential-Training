2d18779955f3e4e9b213df0ed3cb1be0dedb5776
Tore Mæland Hirth <torehirth@gmail.com>
2023-11-15 20:28:40 +0100

    selecting DOM elements using query selector

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,23 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+const pageHeaderParagraph = document.querySelector(".page-header p");
+// console.log(pageHeaderParagraph);
+
+// -------------------------------------------
+
+const backpackFeatures = document.querySelectorAll(".backpack__features li");
+// console.log(backpackFeatures);
+
+backpackFeatures.forEach((i) => {
+  // console.log(i);
+});
+
+// -------------------------------------------
+
+const test = document.querySelector(".backpack__features .backpack__color");
+console.log(test);
+
+test.style.backgroundColor = "pink";
+test.lastChild.innerHTML = "New Colour";
